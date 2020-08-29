1e56414c433e9a746dba78aa54a8790cef61b9f9
JakobLarsson <jakob.larsson@live.se>
2020-08-29 22:47:59 +0200

    ifall / och * anvÃnds prioriteras dom nu efter skriv ordning av dem

--- Program.cs
+++ Program.cs
@@ -16,38 +16,76 @@ namespace Inlamningsuppgift_1
             string opperator2 = Console.ReadLine();
 
             Console.WriteLine("Enter first term");
-            int term1 = Convert.ToInt32(Console.ReadLine());
+            double term1 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Enter second term");
-            int term2 = Convert.ToInt32(Console.ReadLine());
+            double term2 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Enter third term");
-            int term3 = Convert.ToInt32(Console.ReadLine());
-            int answear1 = 0;
-            int answear2 = 0;
+            double term3 = Convert.ToDouble(Console.ReadLine());
+            double answear1 = 0;
+            double answear2 = 0;
 
-            int step1 = 0;
-            int step2;
-            
-            //switch för att prioritera * och / först i uträkningen  sedan uträkning utefter prioritering med hjälp av CheckOpperator
+            double step1 = 0;
+            double step2 = 0;
+            //switch för att prioritera * och / först i uträkningen  sedan uträkning utefter prioritering med hjälp av CheckOpperato
+            //första switch satsen är till för att prioritera bort útifall både * och / skulle användas
 
-            switch (opperator2)
+            bool test = true;
+
+            switch (opperator1)
             {
-                
+                case "+":
+                    switch (opperator2)
+                    {
+
+                        case "*":
+                            step1 = CheckOpperator(term2, term3, opperator2);
+                            step2 = CheckOpperator(step1, term1, opperator1);
+                            break;
+                        case "/":
+                            step1 = CheckOpperator(term2, term3, opperator2);
+                            step2 = CheckOpperator(step1, term1, opperator1);
+                            break;
+                        default:
+                            step1 = CheckOpperator(term1, term2, opperator1);
+                            step2 = CheckOpperator(step1, term3, opperator2);
+                            break;
+
+                    }
+                    break;
+                case "-":
+                    switch (opperator2)
+                    {
+
+                        case "*":
+                            step1 = CheckOpperator(term2, term3, opperator2);
+                            step2 = CheckOpperator(step1, term1, opperator1);
+                            break;
+                        case "/":
+                            step1 = CheckOpperator(term2, term3, opperator2);
+                            step2 = CheckOpperator(step1, term1, opperator1);
+                            break;
+                        default:
+                            step1 = CheckOpperator(term1, term2, opperator1);
+                            step2 = CheckOpperator(step1, term3, opperator2);
+                            break;
+
+                    }
+                    break;
                 case "*":
-                    step1 = CheckOpperator(term2, term3, opperator2);
-                    step2 = CheckOpperator(step1, term1, opperator1);
-                    break;
-                case "/":
-                    step1 = CheckOpperator(term2, term3, opperator2);
-                    step2 = CheckOpperator(step1, term1, opperator1);
-                    break;
-                default:
                     step1 = CheckOpperator(term1, term2, opperator1);
                     step2 = CheckOpperator(step1, term3, opperator2);
                     break;
-        
+                case "/":
+                    step1 = CheckOpperator(term1, term2, opperator1);
+                    step2 = CheckOpperator(step1, term3, opperator2);
+                    break;
+
             }
+
+
+            
             
 
             
@@ -65,30 +103,30 @@ namespace Inlamningsuppgift_1
         }
        
         //metoder för att göra uträkningar med dom olika opperatorerna
-        static int addition(int nr1, int nr2)
+        static double addition(double nr1, double nr2)
         {
-            int result;
+            double result;
             result = nr1 + nr2;
             return result;
         }
 
-        static int subtraktion(int nr1, int nr2)
+        static double subtraktion(double nr1, double nr2)
         {
-            int result;
+            double result;
             result = nr1 - nr2;
             return result;
         }
 
-        static int multiplikation(int nr1, int nr2)
+        static double multiplikation(double nr1, double nr2)
         {
-            int result;
+            double result;
             result = nr1 * nr2;
             return result;
         }
 
-        static int division(int nr1, int nr2)
+        static double division(double nr1, double nr2)
         {
-            int result;
+            double result;
             result = nr1 / nr2;
             return result;
         }
@@ -96,30 +134,30 @@ namespace Inlamningsuppgift_1
         /* CheckOpperator metod för att stega igenom vilken opperator användaren 
          * har valt samt genomföra uträkningen genom att kalla på räkne metoden*/
 
-        static int CheckOpperator(int term1, int term2, string opperator)
+        static double CheckOpperator(double term1, double term2, string opperator)
         {
-            int answear = 0;
+            double answear = 0;
             if (opperator == "*")
             {
-                int summ1 = (multiplikation(term1, term2));
+                double summ1 = (multiplikation(term1, term2));
                 
                 answear = summ1;
             }
             if (opperator == "+")
             {
-                int summ1 = (addition(term1, term2));
+                double summ1 = (addition(term1, term2));
                 
                 answear = summ1;
             }
             if (opperator == "-")
             {
-                int summ1 = (subtraktion(term1, term2));
+                double summ1 = (subtraktion(term1, term2));
                 
                 answear = summ1;
             }
             if (opperator == "/")
             {
-                int summ1 = (division(term1, term2));
+                double summ1 = (division(term1, term2));
                 
                 answear = summ1;
             }
